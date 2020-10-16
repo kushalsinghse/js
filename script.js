30e302642b7830e8428f36510a8d237f217a5202
kushalsinghse <kushalsinghse@gmail.com>
2020-10-16 13:53:51 +0530

    add commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function ans() {
+	var x = document.getElementById('first').value;
+	var y = document.getElementById('second').value;
+	var z = +x + +y;
+	document.getElementById('answer').innerHTML=z;
+
+}
